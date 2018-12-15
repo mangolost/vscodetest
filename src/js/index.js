fb4138e19d8252b383d95454157ee157d60bcdaa
mangolost <lichen0_0@163.com>
2018-12-15 21:29:53 +0800

    rxjs

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,31 @@
+// import Rx from 'rxjs/Rx';
+
 window.onload = function () {
     let $title = document.querySelector("#title");
     let classList = $title.classList;
-    document.querySelector("#btnRed").onclick = function () {
-        classList.remove("blue");
-        classList.add("red");
-    }
-    document.querySelector("#btnBlue").onclick = function () {
-        classList.remove("red");
-        classList.add("blue");
-    }
+    // document.querySelector("#btnRed").onclick = function () {
+    //     classList.remove("blue");
+    //     classList.add("red");
+    // }
+    // document.querySelector("#btnBlue").onclick = function () {
+    //     classList.remove("red");
+    //     classList.add("blue");
+    // }
+
+    let $btnRed = document.querySelector("#btnRed");
+    Rx.Observable
+        .fromEvent($btnRed, 'click')
+        .subscribe(() => {
+            classList.remove("blue");
+            classList.add("red");
+        });
+
+    let $btnBlue = document.querySelector("#btnBlue");
+    Rx.Observable
+        .fromEvent($btnBlue, 'click')
+        .subscribe(() => {
+            classList.remove("red");
+            classList.add("blue");
+        });
+
 }
