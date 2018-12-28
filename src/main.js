01c4520cc8505cde6d41549e095064fee267f28c
mangolost <lichen0_0@163.com>
2018-12-28 21:19:47 +0800

    1111

--- src/main.js
+++ src/main.js
@@ -1,12 +1,34 @@
-let app = require('electron').app;
-let BrowserWindow = require("electron").BrowserWindow;
+const { app, BrowserWindow, Menu, Tray, shell } = require('electron');
 
-// app.commandLine.appendSwitch("--disable-http-cache"); //生产环境需要注释掉
+app.commandLine.appendSwitch("--disable-http-cache"); //生产环境需要注释掉
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow;
+let mainWindow = null;
+let tray = null;
 
+/**
+ * 单实例
+ */
+const gotTheLock = app.requestSingleInstanceLock();
+if (!gotTheLock) {
+    app.quit();
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // Someone tried to run a second instance, we should focus our window.
+        if (mainWindow) {
+            mainWindow.show();
+            if (mainWindow.isMinimized()) {
+                mainWindow.restore();
+            }
+            mainWindow.focus();
+        }
+    });
+}
+
+/**
+ * 
+ */
 function createWindow() {
     /**
      * Initial window options
@@ -15,11 +37,8 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         height: 450,
         width: 800,
-        fullscreen: false,
-        autoHideMenuBar: false,
-        // webPreferences: {
-        //     nativeWindowOpen: true
-        // }
+        autoHideMenuBar: true,
+        show: false
     });
 
     let winURL = `file://${__dirname}/index.html`
@@ -31,6 +50,10 @@ function createWindow() {
     // Close the DevTools.
     mainWindow.webContents.closeDevTools();
 
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show();
+      });
+
     mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
@@ -39,31 +62,35 @@ function createWindow() {
     });
 }
 
-// 关键代码在这里
-/**
- *
- * @type {boolean}
- */
-const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
-    if (mainWindow) {
-        mainWindow.show();
-        if (mainWindow.isMinimized()) {
-            mainWindow.restore();
-        }
-        mainWindow.focus();
-    }
-    return true;
-});
-if (shouldQuit) {
-    app.quit();
-}
-// 关键代码在这里
-
+// Create myWindow, load the rest of the app, etc...
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', function () {
+app.on('ready', () => {
     createWindow();
+    tray = new Tray('src/icons/electron_32px.ico');
+    const contextMenu = Menu.buildFromTemplate([
+        {
+            label: '关于', 
+            type: 'normal', 
+            click: function () {
+                shell.openExternal('https://github.com/');
+            }
+        },
+        {
+            label: '退出', 
+            type: 'normal', 
+            click: function () {
+                app.quit();
+            }
+        }
+    ]);
+    tray.setToolTip('This is my application.')
+    tray.setContextMenu(contextMenu);
+    tray.on('double-click', () => {
+        mainWindow.show();
+        mainWindow.focus();
+    });    
 });
 
 // Quit when all windows are closed.
